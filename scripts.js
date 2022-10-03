343a6602635ff7107a1a8dc460dd869d491eedd1
Matt Kleinsmith <8968171+MattKleinsmith@users.noreply.github.com>
2022-10-02 22:33:51 -0500

    Dry

--- scripts.js
+++ scripts.js
@@ -1,230 +1,79 @@
-document.body.addEventListener("keypress", function (e) {
-  // Middle row
-  if (e.key === "a") {
-    if (play("./piano-notes/4-c.wav")) {
-      return;
-    } else {
-      play("./piano-notes/4-c.wav");
-      document.querySelector(".white-1").classList.add("white-keydown");
-      document.querySelector(".white-1").classList.remove("white");
-    }
-  }
-  if (e.key === "s") {
-    play("./piano-notes/4-d.wav");
-    document.querySelector(".white-2").classList.add("white-keydown");
-    document.querySelector(".white-2").classList.remove("white");
-  }
-  if (e.key === "d") {
-    play("./piano-notes/4-e.wav");
-    document.querySelector(".white-3").classList.add("white-keydown");
-    document.querySelector(".white-3").classList.remove("white");
-  }
-  if (e.key === "f") {
-    play("./piano-notes/4-f.wav");
-    document.querySelector(".white-4").classList.add("white-keydown");
-    document.querySelector(".white-4").classList.remove("white");
-  }
-  if (e.key === "g") {
-    play("./piano-notes/4-g.wav");
-    document.querySelector(".white-5").classList.add("white-keydown");
-    document.querySelector(".white-5").classList.remove("white");
-  }
-  if (e.key === "h") {
-    play("./piano-notes/4-a.wav");
-    document.querySelector(".white-6").classList.add("white-keydown");
-    document.querySelector(".white-6").classList.remove("white");
-  }
-  if (e.key === "j") {
-    play("./piano-notes/4-b.wav");
-    document.querySelector(".white-7").classList.add("white-keydown");
-    document.querySelector(".white-7").classList.remove("white");
-  }
-  if (e.key === "k") {
-    play("./piano-notes/5-c.wav");
-    document.querySelector(".white-8").classList.add("white-keydown");
-    document.querySelector(".white-8").classList.remove("white");
-  }
-  if (e.key === "l") {
-    play("./piano-notes/5-d.wav");
-    document.querySelector(".white-9").classList.add("white-keydown");
-    document.querySelector(".white-9").classList.remove("white");
-  }
-  if (e.key === ";") {
-    play("./piano-notes/5-e.wav");
-    document.querySelector(".white-10").classList.add("white-keydown");
-    document.querySelector(".white-10").classList.remove("white");
-  }
-
-  // Top row
-  if (e.key === "w") {
-    play("./piano-notes/4-cs.wav");
-    document.querySelector(".black-1").classList.add("black-keydown");
-    document.querySelector(".black-1").classList.remove("black");
-  }
-  if (e.key === "e") {
-    play("./piano-notes/4-ds.wav");
-    document.querySelector(".black-2").classList.add("black-keydown");
-    document.querySelector(".black-2").classList.remove("black");
-  }
-  if (e.key === "t") {
-    play("./piano-notes/4-fs.wav");
-    document.querySelector(".black-3").classList.add("black-keydown");
-    document.querySelector(".black-3").classList.remove("black");
-  }
-  if (e.key === "y") {
-    play("./piano-notes/4-gs.wav");
-    document.querySelector(".black-4").classList.add("black-keydown");
-    document.querySelector(".black-4").classList.remove("black");
-  }
-  if (e.key === "u") {
-    play("./piano-notes/4-as.wav");
-    document.querySelector(".black-5").classList.add("black-keydown");
-    document.querySelector(".black-5").classList.remove("black");
-  }
-  if (e.key === "o") {
-    play("./piano-notes/5-cs.wav");
-    document.querySelector(".black-6").classList.add("black-keydown");
-    document.querySelector(".black-6").classList.remove("black");
-  }
-  if (e.key === "p") {
-    play("./piano-notes/5-ds.wav");
-    document.querySelector(".black-7").classList.add("black-keydown");
-    document.querySelector(".black-7").classList.remove("black");
-  }
-});
-function play(path) {
+function playAudio(path) {
   const sound = new Audio();
   const source = document.createElement("source");
   source.src = path;
   sound.appendChild(source);
-  // sound.play();
-  if (sound.paused) {
-    sound.play();
-  } else {
-    sound.pause();
+  sound.play();
+}
+
+class Note {
+  constructor(...params) {
+    [this.inputKey, this.path, this.selector, this.classUp, this.classDown] = params;
+    this.htmlElement = document.querySelector(this.selector);
   }
 
-  document.body.addEventListener("keyup", function () {
-    sound.stop();
-  });
+  play(inputKey) {
+    if (inputKey !== this.inputKey) return;
+    playAudio(this.path);
+    this.lower();
+  }
+
+  lower() {
+    this.htmlElement.classList.add(this.classDown);
+    this.htmlElement.classList.remove(this.classUp);
+  }
+
+  raise(inputKey) {
+    if (inputKey !== this.inputKey) return;
+    this.htmlElement.classList.add(this.classUp);
+    this.htmlElement.classList.remove(this.classDown);
+  }
 }
-const whiteKeys = Array.from(document.querySelectorAll("white"));
-const keys = document.querySelectorAll("div");
-keys.forEach((key) => {
-  key.addEventListener("mousedown", function () {
-    let keyText = key.innerText;
-    if (key.dataset.key === keyText) {
-      key.classList.add("white-keydown");
-    }
-    if (key.dataset.black === keyText) {
-      key.classList.add("black-keydown");
-    }
-    if (keyText === "a".toUpperCase()) play("./piano-notes/4-c.wav");
-    if (keyText === "s".toUpperCase()) play("./piano-notes/4-d.wav");
-    if (keyText === "d".toUpperCase()) play("./piano-notes/4-e.wav");
-    if (keyText === "f".toUpperCase()) play("./piano-notes/4-f.wav");
-    if (keyText === "g".toUpperCase()) play("./piano-notes/4-g.wav");
-    if (keyText === "h".toUpperCase()) play("./piano-notes/4-a.wav");
-    if (keyText === "j".toUpperCase()) play("./piano-notes/4-b.wav");
-    if (keyText === "k".toUpperCase()) play("./piano-notes/5-c.wav");
-    if (keyText === "l".toUpperCase()) play("./piano-notes/5-d.wav");
-    if (keyText === ";".toUpperCase()) play("./piano-notes/5-e.wav");
-    // Top row
-    if (keyText === "w".toUpperCase()) play("./piano-notes/4-cs.wav");
-    if (keyText === "e".toUpperCase()) play("./piano-notes/4-ds.wav");
-    if (keyText === "t".toUpperCase()) play("./piano-notes/4-fs.wav");
-    if (keyText === "y".toUpperCase()) play("./piano-notes/4-gs.wav");
-    if (keyText === "u".toUpperCase()) play("./piano-notes/4-as.wav");
-    if (keyText === "i".toUpperCase()) play("./piano-notes/5-bs.wav");
-    if (keyText === "o".toUpperCase()) play("./piano-notes/5-cs.wav");
-    if (keyText === "p".toUpperCase()) play("./piano-notes/5-ds.wav");
-  });
-  key.addEventListener("mouseup", function (e) {
-    e.stopPropagation();
-    key.classList.remove("white-keydown");
-    key.classList.remove("black-keydown");
-  });
+
+const middleRow = {
+  classUp: "white",
+  classDown: "white-keydown"
+}
+
+const topRow = {
+  classUp: "black",
+  classDown: "black-keydown"
+}
+
+const notes = [
+  // Middle row
+  new Note("a", "./piano-notes/4-c-cut-fade.mp3", ".white-1", middleRow.classUp, middleRow.classDown),
+  new Note("s", "./piano-notes/4-d-cut-fade.mp3", ".white-2", middleRow.classUp, middleRow.classDown),
+  new Note("d", "./piano-notes/4-e-cut-fade.mp3", ".white-3", middleRow.classUp, middleRow.classDown),
+  new Note("f", "./piano-notes/4-f-cut-fade.mp3", ".white-4", middleRow.classUp, middleRow.classDown),
+  new Note("g", "./piano-notes/4-g-cut-fade.mp3", ".white-5", middleRow.classUp, middleRow.classDown),
+  new Note("h", "./piano-notes/4-a-cut-fade.mp3", ".white-6", middleRow.classUp, middleRow.classDown),
+  new Note("j", "./piano-notes/4-b-cut-fade.mp3", ".white-7", middleRow.classUp, middleRow.classDown),
+  new Note("k", "./piano-notes/5-c-cut-fade.mp3", ".white-8", middleRow.classUp, middleRow.classDown),
+  new Note("l", "./piano-notes/5-d-cut-fade.mp3", ".white-9", middleRow.classUp, middleRow.classDown),
+  new Note(";", "./piano-notes/5-e-cut-fade.mp3", ".white-10", middleRow.classUp, middleRow.classDown),
+  // Top row
+  new Note("w", "./piano-notes/4-cs-cut-fade.mp3", ".black-1", topRow.classUp, topRow.classDown),
+  new Note("e", "./piano-notes/4-ds-cut-fade.mp3", ".black-2", topRow.classUp, topRow.classDown),
+  new Note("t", "./piano-notes/4-fs-cut-fade.mp3", ".black-3", topRow.classUp, topRow.classDown),
+  new Note("y", "./piano-notes/4-gs-cut-fade.mp3", ".black-4", topRow.classUp, topRow.classDown),
+  new Note("u", "./piano-notes/4-as-cut-fade.mp3", ".black-5", topRow.classUp, topRow.classDown),
+  new Note("o", "./piano-notes/5-cs-cut-fade.mp3", ".black-6", topRow.classUp, topRow.classDown),
+  new Note("p", "./piano-notes/5-ds-cut-fade.mp3", ".black-7", topRow.classUp, topRow.classDown),
+]
+
+document.addEventListener("keydown", function (e) {
+  notes.forEach(note => note.play(e.key));
 });
 
-const blackKeys = Array.from(document.querySelectorAll("black"));
+document.addEventListener("keyup", function (e) {
+  notes.forEach(note => note.raise(e.key));
+});
 
-document.body.addEventListener("keyup", function (e) {
-  sound.stop();
-  if (e.key === "a") {
-    document.querySelector(".white-1").classList.remove("white-keydown");
-    document.querySelector(".white-1").classList.add("white");
-  }
-  if (e.key === "s") {
-    document.querySelector(".white-2").classList.remove("white-keydown");
-    document.querySelector(".white-2").classList.add("white");
-  }
-  if (e.key === "d") {
-    document.querySelector(".white-3").classList.remove("white-keydown");
-    document.querySelector(".white-3").classList.add("white");
-  }
-  if (e.key === "f") {
-    document.querySelector(".white-4").classList.remove("white-keydown");
-    document.querySelector(".white-4").classList.add("white");
-  }
-  if (e.key === "g") {
-    document.querySelector(".white-5").classList.remove("white-keydown");
-    document.querySelector(".white-5").classList.add("white");
-  }
-  if (e.key === "h") {
-    document.querySelector(".white-6").classList.remove("white-keydown");
-    document.querySelector(".white-6").classList.add("white");
-  }
-  if (e.key === "j") {
-    document.querySelector(".white-7").classList.remove("white-keydown");
-    document.querySelector(".white-7").classList.add("white");
-  }
-  if (e.key === "k") {
-    document.querySelector(".white-8").classList.remove("white-keydown");
-    document.querySelector(".white-8").classList.add("white");
-  }
-  if (e.key === "l") {
-    document.querySelector(".white-9").classList.remove("white-keydown");
-    document.querySelector(".white-9").classList.add("white");
-  }
-  if (e.key === ";") {
-    document.querySelector(".white-10").classList.remove("white-keydown");
-    document.querySelector(".white-10").classList.add("white");
-  }
+document.addEventListener("mousedown", function (e) {
+  notes.forEach(note => note.play(e.target.innerText.toLowerCase()));
+});
 
-  // Top row
-  if (e.key === "w") {
-    play("./piano-notes/4-cs.wav");
-    document.querySelector(".black-1").classList.remove("black-keydown");
-    document.querySelector(".black-1").classList.add("black");
-  }
-  if (e.key === "e") {
-    play("./piano-notes/4-ds.wav");
-    document.querySelector(".black-2").classList.remove("black-keydown");
-    document.querySelector(".black-2").classList.add("black");
-  }
-  if (e.key === "t") {
-    play("./piano-notes/4-fs.wav");
-    document.querySelector(".black-3").classList.remove("black-keydown");
-    document.querySelector(".black-3").classList.add("black");
-  }
-  if (e.key === "y") {
-    play("./piano-notes/4-gs.wav");
-    document.querySelector(".black-4").classList.remove("black-keydown");
-    document.querySelector(".black-4").classList.add("black");
-  }
-  if (e.key === "u") {
-    play("./piano-notes/4-as.wav");
-    document.querySelector(".black-5").classList.remove("black-keydown");
-    document.querySelector(".black-5").classList.add("black");
-  }
-  if (e.key === "o") {
-    play("./piano-notes/5-cs.wav");
-    document.querySelector(".black-6").classList.remove("black-keydown");
-    document.querySelector(".black-6").classList.add("black");
-  }
-  if (e.key === "p") {
-    play("./piano-notes/5-ds.wav");
-    document.querySelector(".black-7").classList.remove("black-keydown");
-    document.querySelector(".black-7").classList.add("black");
-  }
+document.addEventListener("mouseup", function (e) {
+  notes.forEach(note => note.raise(e.target.innerText.toLowerCase()));
 });
